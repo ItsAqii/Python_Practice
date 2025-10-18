d7fd3a1a6b8787abbb4a3fd3d153d5c1a191bc0a
Aqib Ameen <aqibameen790@gmail.com>
2025-10-18 13:45:13 +0500

    Fixed spelling mistakes

--- OOP/OOP_Tutorial_1_class/Class_instances.py
+++ OOP/OOP_Tutorial_1_class/Class_instances.py
@@ -21,21 +21,24 @@ class employee:
 # creating instances of the class
 
 class employee:
-    def __init__(self, name, age, salary, email):
+    def __init__(self, name, last_name, age, salary, email):
         self.name = name
+        self.last_name = last_name
         self.age = age
         self.salary = salary
         self.email = email
+    def full_name(self):
+        return '{} {}'.format(self.name, self.last_name)
 
 # creating instances of the class
 
-emp1 = employee("Aqib", 24, 50000, "aqib.am@gmail.com") # instance 1
+emp1 = employee("Aqib", "Javed", 24, 50000, "aqib.am@gmail.com") # instance 1
 
-emp2 = employee("Ali", 25, 60000, "ali.am@gmail.com")   # instance 2
+emp2 = employee("Ali", "Noor", 25, 60000, "ali.am@gmail.com")   # instance 2
 
 #print(emp1)
 #print(emp2)
-
-print(emp1.salary)
-print(emp2.email)
-
+#print(emp1.salary)
+#print(emp2.email)
+print(emp1.full_name())
+print(emp2.full_name())
--- OOP/OOP_Tutorial_2_Variables/Class_variables.py
+++ OOP/OOP_Tutorial_2_Variables/Class_variables.py
@@ -1,1 +1,24 @@
- 
+class Employee:
+    # Class variable
+    raise_amount = 1.05
+    # Constructor
+    def __init__(self, f_name, l_name, pay):
+        self.f_name = f_name  # Instance variable
+        self.l_name = l_name  # Instance variable
+        self.pay = pay  # Instance variable
+        self.email = f_name + '.' + l_name + '@company.com'     # Instance variable
+
+
+    def fullname(self):
+        return '{} {}'.format(self.f_name, self.l_name)
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
+emp_1 = Employee('Salman', 'Khan', 50000)
+emp_2 = Employee('Shahrukh', 'Khan', 60000)
+print(emp_1.pay)  # Output: 50000
+emp_1.apply_raise()
+print(emp_1.pay)  # Output: 52500 
+
+print(emp_1.__dict__)  # Output: {'f_name': 'Salman', 'l_name': 'Khan', 'pay': 50000, 'email': '
+
